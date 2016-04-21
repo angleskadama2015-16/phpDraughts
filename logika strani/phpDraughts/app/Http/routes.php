c2214e5477b67aa4a9e22e687deb5a9b1042eff1
jurekucer <jure.kucer@student.um.si>
2016-04-21 21:50:17 +0200

    Nova logika strani
    Na novo nareta logika strani z login, registracijo, spreminjanje
    uporabnikovih podatkov, nalaganje profilnih slik...

---
+++ logika strani/phpDraughts/app/Http/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//Routi za akcije na kontrolerje
+Route::auth();
+Route::get('/home', 'HomeController@index');
+Route::get('/about', 'HomeController@about');
+Route::get('/rules', 'HomeController@rules');
+Route::get('/play', 'HomeController@play');
+Route::get('/users', 'HomeController@users');
+Route::get('/edit', 'HomeController@edit');
+Route::post('/change', 'HomeController@change');
---
+++ logika strani/phpDraughts/resources/views/userPages/successEdit.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <meta http-equiv="refresh" content="3; url=../public" />
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-body">
+                        <span class="bg-success">Profile successfully edited! redirecting back in 3 seconds!</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
